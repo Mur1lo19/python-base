b72b162dc5e7f5cc116fa454d17c926d0717c720
murilo ferreira <muriloft98@gmail.com>
2023-09-24 16:33:04 -0300

    Configurar LANG e metadados

--- hello.py
+++ hello.py
@@ -1,3 +1,35 @@
 #!/usr/bin/env python3
-# Este programa imprime Hello World!!!
-print("Hello, World!") 
+"""
+Hello word mult linguas.
+
+Dependendo da lingua configurada no ambiente o programa exibe a mensagem
+correspondente.
+
+Como usar:
+Tenha váriavel LANG devidamente configurada ex:
+
+    export LANG=pt_BR
+
+Execução:
+
+    python hello.py
+    ou
+    ./hello.py
+"""
+__version__ = "0.0.1"
+__author__ = "Murilo Ferreira"
+__license__ = "unlicense"
+
+import os
+
+current_language = os.getenv("LANG", "pt_BR")[:5]
+
+msg = "Hello, World!"
+
+if current_language == "pt_BR":
+    msg = "Olá, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+
+print(msg)
+
